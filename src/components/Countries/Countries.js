30024c7af59b683580ec1ebd416ead82270dffcf
Rumana K <rumana2k20@gmail.com>
2022-03-21 01:36:39 +0600

    CSS Added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setCountries(data))
+    }, [])
     return (
-        <div>
-           
+        <div >
+
             <h1>All Countries List</h1>
             <h2>Total Countries: {countries.length}</h2>
-            {countries.map(country => 
-            
-            <Country country={country} ></Country>
-            
-            // Option: 1
-            // <Country name={country.name.common} area={country.area} population={country.population} ></Country>
-            )
-            }
+
+            <div className='countries'>
+                {countries.map(country =>
+
+                    <Country country={country} ></Country>
+
+                    // Option: 1
+                    // <Country name={country.name.common} area={country.area} population={country.population} ></Country>
+                )}
+            </div>
+
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Country.css'
 const Country = (props) => {
     const {name, area, population } = props.country
     return (
-        <div>
+        <div className='country'>
             <h3>Country Name: {name.common}</h3>
             <p>Area: {area} </p>
             <p>Population: {population}</p>
